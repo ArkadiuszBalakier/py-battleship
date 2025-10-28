54309f3d104755ec6cbebee5b90521a11743d05e
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-10-28 21:56:05 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -4,31 +4,83 @@ class Deck:
 
 
 class Ship:
-    def __init__(self, start, end, is_drowned=False):
-        # Create decks and save them to a list `self.decks`
-        pass
+    def __init__(self, start: tuple, end: tuple, is_drowned=False):
+        ship_cords = set()
+        r1, c1 = start
+        r2, c2 = end
 
-    def get_deck(self, row, column):
-        # Find the corresponding deck in the list
-        pass
+        if r1 == r2:
+            ship_cords = {(r1, c) for c in range(min(c1, c2), max(c1, c2) + 1)}
+        elif c1 == c2:
+            ship_cords = {(r, c1) for r in range(min(r1, r2), max(r1, r2) + 1)}
+        if not ship_cords:
+            raise ValueError("Ship must be horizontal or vertical")
 
-    def fire(self, row, column):
-        # Change the `is_alive` status of the deck
-        # And update the `is_drowned` value if it's needed
-        pass
+        self.cords = ship_cords
+        self.hits = set()
+        self.is_drowned = is_drowned
+        self.size = len(self.cords)
+
+    def is_sunk(self) -> str:
+        self.is_drowned = True
+        return "Sunk!"
+
+
+    def get_deck(self, row, column) -> bool:
+        if (row, column) in self.cords:
+            return True
+        return False
+
+    def fire(self, row, column) -> str:
+        if self.get_deck(row, column):
+            self.hits.add((row, column))
+            if self.hits == self.cords:
+                return self.is_sunk()
+            else:
+                return "Hit!"
+        return "Miss!"
 
 
 class Battleship:
     def __init__(self, ships):
-        # Create a dict `self.field`.
-        # Its keys are tuples - the coordinates of the non-empty cells,
-        # A value for each cell is a reference to the ship
-        # which is located in it
-        pass
+        self.ships = []
+        self.field = {}
+        for start, end in ships:
+            cords = (start, end)
+            if cords in self.field:
+                raise ValueError("Overlapping ships")
+            if  0 <= start < 10 and 0 <= end < 10:
+                ship = Ship(start, end)
+                self.ships.append(ship)
+            else:
+                raise ValueError("Ship out of board")
 
     def fire(self, location: tuple):
-        # This function should check whether the location
-        # is a key in the `self.field`
-        # If it is, then it should check if this cell is the last alive
-        # in the ship or not.
-        pass
+        if not isinstance(location, tuple) and not len(location) == 2:
+            return "Miss!"
+
+        if location not in self.field:
+            return "Mis!"
+
+        ship = self.field[location]
+        result = ship.fire(*location)
+        return result
+
+    def _validate_field(self) -> None:
+        expected_ship_count = {
+            4:1,
+            3:2,
+            2:3,
+            1:4
+        }
+
+        for ship in self.ships:
+            if ship.size not in expected_ship_count:
+                raise ValueError("Invalid ship size")
+        actual_ship_counts = {}
+        for ship in self.ships:
+            size = ship.size
+            actual_ship_counts[size] = actual_ship_counts.get(size, 0) + 1
+
+        if actual_ship_counts != expected_ship_count:
+            raise ValueError("Invalid ship count")
